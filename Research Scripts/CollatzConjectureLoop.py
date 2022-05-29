769a85cc960f289071568c9c0d35ec5bf2af631e
itsallfineandandy <andyjhaley@gmail.com>
2022-05-29 22:22:59 +0100

    Interations calculating correctly. csv created without index col.

--- Research Scripts/CollatzConjectureLoop.py
+++ Research Scripts/CollatzConjectureLoop.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-df = pd.DataFrame(columns=['I', 'Interations', 'Max', 'Min'])
+df = pd.DataFrame(columns=['I', 'Interations', 'Max'])
 
 if (os.path.isfile('/Users/andrewhaley/Github Repos/Research Projects/Collatz-Conjecture/Data/LatestProgressLoop.csv')):
   df = pd.read_csv('/Users/andrewhaley/Github Repos/Research Projects/Collatz-Conjecture/Data/LatestProgressLoop.csv')
@@ -9,12 +9,10 @@ if (os.path.isfile('/Users/andrewhaley/Github Repos/Research Projects/Collatz-Co
 i = df['I'].max()
 if(pd.isnull(i)):
   i = 1
-while(i < 1000000000000000):
+while(i < 100000000001):
   test = i
-  i = i+1
   interations = 0
   MaxNum = i
-  MinNum = i
   while(test != 1):
     if test % 2 == 0:
       test = test/2
@@ -22,9 +20,10 @@ while(i < 1000000000000000):
       test = test*3+1
     interations = interations+1
     MaxNum = max(MaxNum,test)
-    MinNum = min(MinNum,test)
-  df = pd.concat([df, pd.DataFrame.from_records([{'I': i, 'Interations': interations, 'Max': MaxNum, 'Min': MinNum}])])
+  df = pd.concat([df, pd.DataFrame.from_records([{'I': i, 'Interations': interations, 'Max': MaxNum}])])
   if i % 10000 == 0:
-    df.to_csv('/Users/andrewhaley/Github Repos/Research Projects/Collatz-Conjecture/Data/LatestProgressLoop.csv')
-  print("I = ", str(i)," Interations = ",str(interations)," Max = ",str(MaxNum)," Min = ",str(MinNum))
+    df.to_csv('/Users/andrewhaley/Github Repos/Research Projects/Collatz-Conjecture/Data/LatestProgressLoop.csv', index=False)
+  print("I = ", str(i)," Interations = ",str(interations)," Max = ",str(MaxNum))
+  i = i+1
 
+print(df)
